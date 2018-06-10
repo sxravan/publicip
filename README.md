593a627a88f21a186a26ec9a776df6c1bd8249f6
root <root@shravanpi>
2018-06-10 11:19:30 +0000

    Changed template

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 3. Paste the path in the script. (Assign it to the IPFILE variable). `IPFILE = ~/ip.txt`
 4. Enter your e-mail address in the command found below the template.
 5. Install `mailutils`. `sudo apt-get install mailutils`
-6. Configure postfix `vim /etc/postfix`.
+6. Configure postfix `vim /etc/postfix/main.cf`.
 7. Use a proper relay host.
 8. Update your cron as you wish.
--- publicip.sh
+++ publicip.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 #script to send my public ip on mail if changed
 #author=shravandwarka
-#version=0.1.1
+#version=0.2
 
-IPFILE=<enter a text file path here>
+IPFILE=ip
 
 if [ ! -f "$IPFILE" ]; then
-	curl -o ip.txt ipinfo.io/ip
+	curl -o ip ipinfo.io/ip
 fi
 
-CIP=$(cat $IPFILE)
+CIP=$(cat "$IPFILE")
 NIP=""
+REG=""
+COU=""
 
 while [ -z "$NIP" ]
 do
-	NIP=$(curl ipinfo.io/ip)
+	NIP="$(curl ipinfo.io/ip)"
+	REG="$(curl ipinfo.io/region)"
+	COU="$(curl ipinfo.io/country)"
 done
 
 if [ "$NIP" != "$CIP" ]; then
-	ipadd=`cat << TEMPLATE
+	ipadd=$(cat << TEMPLATE
 	<html>
-		<body style="width:75%;margin-left:auto;margin-right:auto;">
-			<table style="width:100%;">
+		<header>
+			<h1 style="font-family: Impact; font-size: 20px; font-weight: bold; text-align: center;">
+				Location: $REG, $COU
+			</h1>
+		</header>
+		<body style="background-color: beige; width:75%;margin-left:auto;margin-right:auto;">
+			<table style="width:100%;margin-top:auto;margin-botton:auto;padding-top:50px;padding-bottom:50px;">
+				<tr>
+					<td colspan="2" style="height: 20px; font-family: 'Lucida Console'; font-size: 15px; font-weight: bold; text-align: center;">
+						Public IP Updater
+					</td>
+				</tr>
 				<tr>
 					<td style="text-align: center;font-family: 'Lucida Console';font-size: 15px;">
 						Old IP Address:
@@ -41,9 +55,12 @@ if [ "$NIP" != "$CIP" ]; then
 
 			</table>
 		</body>
+		<footer style="text-align: right;padding-top:20px;">
+			Crafted by <a href="https://github.com/shravanSD/">Shravan Dwarka</a> in Mauritius.
+		</footer>
 	</html>
 TEMPLATE
-`	
-	/usr/bin/printf "%b" "$ipadd" | /usr/bin/mail -a "Content-Type:text/html;" -a "Mime-Version: 1.0;" -s "Public IP Address" <your e-mail address goes here>
-	curl -o ip.txt ipinfo.io/ip
+)	
+	/usr/bin/printf "%b" "$ipadd" | /usr/bin/mail -a "Content-Type:text/html;" -a "Mime-Version: 1.0;" -s "Public IP Address" <Enter your email here>
+	curl -o ip ipinfo.io/ip
 fi
